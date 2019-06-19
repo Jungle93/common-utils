7c82967b675954b1a5e71152d01c94c413a2be77
Jungle93 <1045340064@qq.com>
2019-06-19 17:10:17 +0800

    1、【新增】新增，添加RSA串签名以及校验的方法，并添加相应测试；

--- src/main/java/com/jungle/common/utils/RSAUtils.java
+++ src/main/java/com/jungle/common/utils/RSAUtils.java
@@ -14,6 +14,8 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
@@ -27,6 +29,11 @@ public final class RSAUtils {
      * 加密算法RSA。
      */
     private static final String KEY_ALGORITHM = "RSA";
+    /**
+     * 签名算法。
+     */
+    private static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
+
     /**
      * 获取公钥的key。
      */
@@ -54,12 +61,98 @@ public final class RSAUtils {
     private RSAUtils() {/**/ }
 
     /**
+     * RSA 验签。
+     *
+     * @param publicKeyStr   Base64编码公钥串
+     * @param dataBytes      数据内容
+     * @param signatureBytes 签名
+     *
+     * @return 验签结果：true-成功；false-失败。
+     *
+     * @throws NoSuchAlgorithmException 没有这个算法
+     * @throws InvalidKeySpecException  非法Key标准
+     * @throws InvalidKeyException      非法key
+     * @throws SignatureException       签名异常
+     */
+    public static boolean signatureVerify(String publicKeyStr, byte[] dataBytes, byte[] signatureBytes) throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+
+        PublicKey publicKey = getPublicKey(publicKeyStr);
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initVerify(publicKey);
+        signature.update(dataBytes);
+        return signature.verify(signatureBytes);
+    }
+
+    /**
+     * 对Base64编码数据内容进行验签。
+     *
+     * @param publicKeyStr Base64公钥串
+     * @param data         数据串
+     * @param signature    Base64签名串 参见{@link #signature(byte[], String)}方法的结果为Base64编码的签名串
+     *
+     * @return 验签结果：true-成功；false-失败。
+     *
+     * @throws NoSuchAlgorithmException 没有这个算法
+     * @throws InvalidKeySpecException  非法Key标准
+     * @throws InvalidKeyException      非法key
+     * @throws SignatureException       签名异常
+     */
+    public static boolean signatureVerify(String publicKeyStr, String data, String signature) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, SignatureException {
+
+        return signatureVerify(publicKeyStr, data.getBytes(), decode(signature));
+    }
+
+    /**
+     * RSA签名。
+     *
+     * @param data          内容数据
+     * @param privateKeyStr 私钥字符串
+     *
+     * @return String
+     *
+     * @throws NoSuchAlgorithmException 没有这个算法
+     * @throws InvalidKeySpecException  非法Key标准
+     * @throws InvalidKeyException      非法key
+     * @throws SignatureException       签名异常
+     */
+    public static String signature(byte[] data, String privateKeyStr) throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+
+        PrivateKey privateKey = getPrivateKey(privateKeyStr);
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initSign(privateKey);
+        signature.update(data);
+        byte[] signBytes = signature.sign();
+        return encode(signBytes);
+    }
+
+    /**
+     * 对Base64串的数据进行RSA签名。
+     *
+     * @param data          Base64数据内容串
+     * @param privateKeyStr Base64编码的私钥串
+     *
+     * @return String
+     *
+     * @throws InvalidKeySpecException  非法Key标准
+     * @throws NoSuchAlgorithmException 没有这个算法
+     * @throws InvalidKeyException      非法key
+     * @throws SignatureException       签名异常
+     */
+    public static String signature(String data, String privateKeyStr) throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+
+        return signature(decode(data), privateKeyStr);
+    }
+
+
+    /**
      * 生成公私钥配对。
      *
      * @return {@link Map}
+     *
      * @throws NoSuchAlgorithmException 没有这个加密算法
      */
     public static Map<String, String> generateKeyPair() throws NoSuchAlgorithmException {
+
         KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
         keyPairGen.initialize(KEY_BIT_SIZE);
         KeyPair keyPair = keyPairGen.generateKeyPair();
@@ -73,6 +166,7 @@ public final class RSAUtils {
      * 二进制数据编码为BASE64字符串。
      */
     public static String encode(byte[] bytes) {
+
         return new String(Base64.getEncoder().encode(bytes));
     }
 
@@ -80,6 +174,7 @@ public final class RSAUtils {
      * BASE64字符串解码为二进制数据。
      */
     public static byte[] decode(String base64) {
+
         return Base64.getDecoder().decode(base64.getBytes());
     }
 
@@ -87,11 +182,14 @@ public final class RSAUtils {
      * 获取公钥。
      *
      * @param publicKeyStr 公钥字符串
+     *
      * @return {@link PublicKey}
+     *
      * @throws InvalidKeySpecException  非法Key标准
      * @throws NoSuchAlgorithmException 没有这个算法
      */
     private static PublicKey getPublicKey(String publicKeyStr) throws InvalidKeySpecException, NoSuchAlgorithmException {
+
         byte[] keyBytes = decode(publicKeyStr);
         X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
@@ -103,7 +201,9 @@ public final class RSAUtils {
      *
      * @param data      待加密数据
      * @param publicKey 公钥字符串
+     *
      * @return 加密后数据
+     *
      * @throws NoSuchAlgorithmException  没有这个算法
      * @throws InvalidKeySpecException   非法Key标准
      * @throws NoSuchPaddingException    没有此填充方式
@@ -113,6 +213,7 @@ public final class RSAUtils {
      * @throws IOException               IO异常
      */
     public static byte[] encryptByPublicKey(byte[] data, String publicKey) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException {
+
         Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, getPublicKey(publicKey));
         return splitCipher(cipher, Cipher.ENCRYPT_MODE, data);
@@ -123,7 +224,9 @@ public final class RSAUtils {
      *
      * @param encryptedData 待解密数据
      * @param publicKey     公钥字符串
+     *
      * @return 解密后数据
+     *
      * @throws NoSuchAlgorithmException  没有这个算法
      * @throws InvalidKeySpecException   非法Key标准
      * @throws NoSuchPaddingException    没有此填充方式
@@ -133,6 +236,7 @@ public final class RSAUtils {
      * @throws IOException               IO异常
      */
     public static byte[] decryptByPublicKey(byte[] encryptedData, String publicKey) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException {
+
         Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, getPublicKey(publicKey));
         return splitCipher(cipher, Cipher.DECRYPT_MODE, encryptedData);
@@ -142,11 +246,14 @@ public final class RSAUtils {
      * 获取私钥。
      *
      * @param privateKeyStr 私钥字符串
+     *
      * @return {@link PrivateKey}
+     *
      * @throws NoSuchAlgorithmException 没有这个算法
      * @throws InvalidKeySpecException  非法Key标准
      */
     private static PrivateKey getPrivateKey(String privateKeyStr) throws NoSuchAlgorithmException, InvalidKeySpecException {
+
         byte[] keyBytes = decode(privateKeyStr);
         PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
@@ -158,7 +265,9 @@ public final class RSAUtils {
      *
      * @param encryptedData 待解密数据
      * @param privateKey    私钥字符串
+     *
      * @return 解密后数据
+     *
      * @throws NoSuchAlgorithmException  没有这个算法
      * @throws InvalidKeySpecException   非法Key标准
      * @throws NoSuchPaddingException    没有此填充方式
@@ -168,6 +277,7 @@ public final class RSAUtils {
      * @throws IOException               IO异常
      */
     public static byte[] decryptByPrivateKey(byte[] encryptedData, String privateKey) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException {
+
         Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, getPrivateKey(privateKey));
         return splitCipher(cipher, Cipher.DECRYPT_MODE, encryptedData);
@@ -178,7 +288,9 @@ public final class RSAUtils {
      *
      * @param data       待加密数据
      * @param privateKey 私钥字符串
+     *
      * @return 加密后数据
+     *
      * @throws NoSuchAlgorithmException  没有这个算法
      * @throws InvalidKeySpecException   非法Key标准
      * @throws NoSuchPaddingException    没有此填充方式
@@ -188,6 +300,7 @@ public final class RSAUtils {
      * @throws IOException               IO异常
      */
     public static byte[] encryptByPrivateKey(byte[] data, String privateKey) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, IOException, InvalidKeyException {
+
         Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, getPrivateKey(privateKey));
         return splitCipher(cipher, Cipher.ENCRYPT_MODE, data);
@@ -199,12 +312,15 @@ public final class RSAUtils {
      * @param cipher 加密器
      * @param mode   方式：加密或者解密{@link Cipher#DECRYPT_MODE} | {@link Cipher#ENCRYPT_MODE}
      * @param data   数据
+     *
      * @return byte[]
+     *
      * @throws BadPaddingException       非法填充
      * @throws IllegalBlockSizeException 非法块大小
      * @throws IOException               IO异常
      */
     private static byte[] splitCipher(Cipher cipher, int mode, byte[] data) throws BadPaddingException, IllegalBlockSizeException, IOException {
+
         int inputLength = data.length;
         int offSet = 0;
         int index = 0;
--- src/test/java/com/jungle/common/utils/RSAUtilsTest.java
+++ src/test/java/com/jungle/common/utils/RSAUtilsTest.java
@@ -6,14 +6,17 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Map;
 import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class RSAUtilsTest {
 
@@ -36,6 +39,22 @@ public class RSAUtilsTest {
         LOGGER.info("private key:" + privateKey);
     }
 
+    @Test
+    public void signature() throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, SignatureException {
+
+        String privateKey = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBALXEhOfRjFjlf0amn335/pV15uGm0jPDhKYpN0Qgxd+2iVK+Rv/qguwhGGYu/r3Z7ck/9xmXdpm/Fq1ncwmMEJ4u1aaZ/7Yd+wfom1ePIK7wkrZnhTYNAlaLNt53UkZjJIdeCTy6pRW12SdevVNjnK7dLm2COq1MjCphowM2ld1vAgMBAAECgYEAmce3gcEhchxHv+g8S4JzxtNHEGXotO6XjVSVc0ge4JQ4Pg7JVr57IXcmg10DHHAB4yAA2F/ycUTf1LkO9GbRrvSgT9HQBBh/R8fwWOYGrcdJYCangp3aNsBFLv+c4n9i582mhO/zq0gwc92ehirQ+b0mCon3nuJCYgXdkCpLeQECQQDnSnu5HM+hEikUm3Cct07shIknhc8x8ZmTM6dKQgf99BvZy+LYvqkkEvHgzg+hK8zfcN4gSAm8yvnGF85eHmtBAkEAyS+izXVT378MIn/BUNe00FHr6JjxPhbW1OLML07SzmWyJX7DdtX7RC4vl0feL6+ED2pvZ41fZSkenkKzA2OMrwJBAIWRfwe0+rbi/PRYm9z1A1AVbft0C+akxnK61rZMkb73tQm+878FxDn/8s1HUsFZwROcY7qyv2TWtw3N+7Bg0sECQQCCqD8w89uu2eJFnXaszAoPqMRu4LT7v9VxmIFG0Ag0JvOVSkgjK0DtvJkHxJLVoUVAxbzoIXYsupG58lNUBhjdAkBwGgWQAckSf8I2mJixDhZniSEtBWtXR6W/3m/DfBB2zURXdOJw6Y1U7/1BiRIQjz98npbvvcPxzMnE7QzqCWM+";
+        String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC1xITn0YxY5X9Gpp99+f6VdebhptIzw4SmKTdEIMXftolSvkb/6oLsIRhmLv692e3JP/cZl3aZvxatZ3MJjBCeLtWmmf+2HfsH6JtXjyCu8JK2Z4U2DQJWizbed1JGYySHXgk8uqUVtdknXr1TY5yu3S5tgjqtTIwqYaMDNpXdbwIDAQAB";
+        String signature1 = RSAUtils.signature("Hello from jungle".getBytes(), privateKey);
+        assertNotNull(signature1);
+        String signature2 = RSAUtils.signature(RSAUtils.encode("Hello from jungle".getBytes()), privateKey);
+        assertNotNull(signature2);
+        assertEquals(signature1,signature2);
+
+        assertTrue(RSAUtils.signatureVerify(publicKey, "Hello from jungle".getBytes(), RSAUtils.decode(signature1)));
+        assertTrue(RSAUtils.signatureVerify(publicKey, "Hello from jungle", signature1));
+
+    }
+
     /**
      * public key [MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC1xITn0YxY5X9Gpp99+f6VdebhptIzw4SmKTdEIMXftolSvkb/6oLsIRhmLv692e3JP/cZl3aZvxatZ3MJjBCeLtWmmf+2HfsH6JtXjyCu8JK2Z4U2DQJWizbed1JGYySHXgk8uqUVtdknXr1TY5yu3S5tgjqtTIwqYaMDNpXdbwIDAQAB]
      * private key [MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBALXEhOfRjFjlf0amn335/pV15uGm0jPDhKYpN0Qgxd+2iVK+Rv/qguwhGGYu/r3Z7ck/9xmXdpm/Fq1ncwmMEJ4u1aaZ/7Yd+wfom1ePIK7wkrZnhTYNAlaLNt53UkZjJIdeCTy6pRW12SdevVNjnK7dLm2COq1MjCphowM2ld1vAgMBAAECgYEAmce3gcEhchxHv+g8S4JzxtNHEGXotO6XjVSVc0ge4JQ4Pg7JVr57IXcmg10DHHAB4yAA2F/ycUTf1LkO9GbRrvSgT9HQBBh/R8fwWOYGrcdJYCangp3aNsBFLv+c4n9i582mhO/zq0gwc92ehirQ+b0mCon3nuJCYgXdkCpLeQECQQDnSnu5HM+hEikUm3Cct07shIknhc8x8ZmTM6dKQgf99BvZy+LYvqkkEvHgzg+hK8zfcN4gSAm8yvnGF85eHmtBAkEAyS+izXVT378MIn/BUNe00FHr6JjxPhbW1OLML07SzmWyJX7DdtX7RC4vl0feL6+ED2pvZ41fZSkenkKzA2OMrwJBAIWRfwe0+rbi/PRYm9z1A1AVbft0C+akxnK61rZMkb73tQm+878FxDn/8s1HUsFZwROcY7qyv2TWtw3N+7Bg0sECQQCCqD8w89uu2eJFnXaszAoPqMRu4LT7v9VxmIFG0Ag0JvOVSkgjK0DtvJkHxJLVoUVAxbzoIXYsupG58lNUBhjdAkBwGgWQAckSf8I2mJixDhZniSEtBWtXR6W/3m/DfBB2zURXdOJw6Y1U7/1BiRIQjz98npbvvcPxzMnE7QzqCWM+]
@@ -54,14 +73,8 @@ public class RSAUtilsTest {
     }
 
     @Test
-    public void decryptByPublicKey() {
-
-
-
-    }
-
-    @Test
     public void decryptByPrivateKey() throws NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidKeyException, InvalidKeySpecException {
+
         String privateKey = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBALXEhOfRjFjlf0amn335/pV15uGm0jPDhKYpN0Qgxd+2iVK+Rv/qguwhGGYu/r3Z7ck/9xmXdpm/Fq1ncwmMEJ4u1aaZ/7Yd+wfom1ePIK7wkrZnhTYNAlaLNt53UkZjJIdeCTy6pRW12SdevVNjnK7dLm2COq1MjCphowM2ld1vAgMBAAECgYEAmce3gcEhchxHv+g8S4JzxtNHEGXotO6XjVSVc0ge4JQ4Pg7JVr57IXcmg10DHHAB4yAA2F/ycUTf1LkO9GbRrvSgT9HQBBh/R8fwWOYGrcdJYCangp3aNsBFLv+c4n9i582mhO/zq0gwc92ehirQ+b0mCon3nuJCYgXdkCpLeQECQQDnSnu5HM+hEikUm3Cct07shIknhc8x8ZmTM6dKQgf99BvZy+LYvqkkEvHgzg+hK8zfcN4gSAm8yvnGF85eHmtBAkEAyS+izXVT378MIn/BUNe00FHr6JjxPhbW1OLML07SzmWyJX7DdtX7RC4vl0feL6+ED2pvZ41fZSkenkKzA2OMrwJBAIWRfwe0+rbi/PRYm9z1A1AVbft0C+akxnK61rZMkb73tQm+878FxDn/8s1HUsFZwROcY7qyv2TWtw3N+7Bg0sECQQCCqD8w89uu2eJFnXaszAoPqMRu4LT7v9VxmIFG0Ag0JvOVSkgjK0DtvJkHxJLVoUVAxbzoIXYsupG58lNUBhjdAkBwGgWQAckSf8I2mJixDhZniSEtBWtXR6W/3m/DfBB2zURXdOJw6Y1U7/1BiRIQjz98npbvvcPxzMnE7QzqCWM+";
         byte[] dataBytes = RSAUtils.decode("jurcmsGRiu7obFLrwmsNPsIABG1xXUwKAo9157oRKspHUswNEXBDL0IOFzPJfF1RoxGp070FlDv7LT+QIBt8fH0btH8m+TuiWx0b5xLy5EHmpckilmMwIsivTxrmUSV1HlsoYwLz+dJac31cnYWztcMqhY0TLNRMAvreWuDzS/g=");
         String clearText = new String(RSAUtils.decryptByPrivateKey(dataBytes, privateKey));
@@ -70,8 +83,22 @@ public class RSAUtilsTest {
     }
 
     @Test
+    public void decryptByPublicKey() throws NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidKeyException, InvalidKeySpecException {
+
+        byte[] bytes = RSAUtils.decode("TU/3intjAKkm7gslX1vh8Lk3yBepRvSJEhLh7xIMpdalXFqndv7+uHNOCJidYaDOeUfIrVGnoa8G8yMPTWeeFX+++kFgnZYKmxHp2IPPXYKhkOzjavNvOXxy/N7tAGlTlB8bhkZ9ot3Cig2V3wilG8ez7tTwVKbv6teXmyKrt0c=");
+        String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC1xITn0YxY5X9Gpp99+f6VdebhptIzw4SmKTdEIMXftolSvkb/6oLsIRhmLv692e3JP/cZl3aZvxatZ3MJjBCeLtWmmf+2HfsH6JtXjyCu8JK2Z4U2DQJWizbed1JGYySHXgk8uqUVtdknXr1TY5yu3S5tgjqtTIwqYaMDNpXdbwIDAQAB";
+        byte[] decryptBytes = RSAUtils.decryptByPublicKey(bytes, publicKey);
+        String clearText = new String(decryptBytes, Charset.forName("utf-8"));
+        assertEquals("Hello from jungle!", clearText);
+
+    }
+
+    @Test
     public void encryptByPrivateKey() throws NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidKeyException, InvalidKeySpecException {
+
         String privateKey = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBALXEhOfRjFjlf0amn335/pV15uGm0jPDhKYpN0Qgxd+2iVK+Rv/qguwhGGYu/r3Z7ck/9xmXdpm/Fq1ncwmMEJ4u1aaZ/7Yd+wfom1ePIK7wkrZnhTYNAlaLNt53UkZjJIdeCTy6pRW12SdevVNjnK7dLm2COq1MjCphowM2ld1vAgMBAAECgYEAmce3gcEhchxHv+g8S4JzxtNHEGXotO6XjVSVc0ge4JQ4Pg7JVr57IXcmg10DHHAB4yAA2F/ycUTf1LkO9GbRrvSgT9HQBBh/R8fwWOYGrcdJYCangp3aNsBFLv+c4n9i582mhO/zq0gwc92ehirQ+b0mCon3nuJCYgXdkCpLeQECQQDnSnu5HM+hEikUm3Cct07shIknhc8x8ZmTM6dKQgf99BvZy+LYvqkkEvHgzg+hK8zfcN4gSAm8yvnGF85eHmtBAkEAyS+izXVT378MIn/BUNe00FHr6JjxPhbW1OLML07SzmWyJX7DdtX7RC4vl0feL6+ED2pvZ41fZSkenkKzA2OMrwJBAIWRfwe0+rbi/PRYm9z1A1AVbft0C+akxnK61rZMkb73tQm+878FxDn/8s1HUsFZwROcY7qyv2TWtw3N+7Bg0sECQQCCqD8w89uu2eJFnXaszAoPqMRu4LT7v9VxmIFG0Ag0JvOVSkgjK0DtvJkHxJLVoUVAxbzoIXYsupG58lNUBhjdAkBwGgWQAckSf8I2mJixDhZniSEtBWtXR6W/3m/DfBB2zURXdOJw6Y1U7/1BiRIQjz98npbvvcPxzMnE7QzqCWM+";
-        RSAUtils.encryptByPrivateKey("Hello from jungle!".getBytes(),privateKey);
+        byte[] bytes = RSAUtils.encryptByPrivateKey("Hello from jungle!".getBytes(), privateKey);
+        String encryptText = RSAUtils.encode(bytes);
+        assertEquals("TU/3intjAKkm7gslX1vh8Lk3yBepRvSJEhLh7xIMpdalXFqndv7+uHNOCJidYaDOeUfIrVGnoa8G8yMPTWeeFX+++kFgnZYKmxHp2IPPXYKhkOzjavNvOXxy/N7tAGlTlB8bhkZ9ot3Cig2V3wilG8ez7tTwVKbv6teXmyKrt0c=", encryptText);
     }
 }
